67b2d9e6a2c088f2710fbfeafb990dfba03fdf06
Tigran <t.asatryan96@gmail.com>
2023-07-20 13:45:54 +0400

    tabs added error persists

--- ui.r
+++ ui.r
@@ -3,10 +3,17 @@ ui <- fluidPage(
   titlePanel("TCX Data Analysis"),
   sidebarLayout(
     sidebarPanel(
-      fileInput("file", "Upload TCX file")
+        fileInput("file", "Upload TCX file"),
+        style = "display: block;"
     ),
+    
     mainPanel(
-      plotOutput("distance_plot")
+      tabsetPanel(id = "tabs",
+                  type = "tabs",
+                  tabPanel("MapMyWalk data Analysis", plotOutput("distance_plot")),
+                  tabPanel("Walkability score calculator", plotOutput("plot1")),
+                  tabPanel("Custom analysis TBD", plotOutput("plot")))
+      
     )
   )
 )
