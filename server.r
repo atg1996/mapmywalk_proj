859c2159ac993062ccad0b2c7599c8cf7a34788e
Tigran <t.asatryan96@gmail.com>
2023-07-24 01:58:15 +0400

    map added

--- server.r
+++ server.r
@@ -11,8 +11,38 @@ parseTCXData <- function(file) {
   return(data)
 }
 
+readMapMyWalkCSV <- function(file_path) {
+  df <- read.csv(file_path, stringsAsFactors = FALSE)
+  return(df)
+}
+
+# Function to extract GPS data from MapMyWalk data
+extractGPSData <- function(data) {
+  gps_data <- data %>%
+    select(latitude, longitude) %>%
+    filter(!is.na(latitude) & !is.na(longitude))
+  
+  return(gps_data)
+}
+
 # Define Server
 server <- function(input, output) {
+  observeEvent(input$file, {
+    req(input$file$datapath)  # Check if a file is uploaded
+    tcx_data <- readTCX(input$file$datapath)  # Replace readTCX with your own function to read TCX data
+    gps_data <- extractGPSData(tcx_data)  # Replace extractGPSData with your own function to extract GPS data
+    
+    # Plot the GPS data on the map
+    output$map <- renderLeaflet({
+      leaflet() %>%
+        addProviderTiles(provider = "OpenStreetMap") %>%
+        addPolylines(data = gps_data, 
+                     lat = ~latitude, 
+                     lng = ~longitude, 
+                     color = "red", 
+                     weight = 3)
+    })
+  })
   
   # Generate distance over time plot
   output$distance_plot <- renderPlot({
--- ui.r
+++ ui.r
@@ -1,12 +1,22 @@
-
-ui <- fluidPage(
-  titlePanel("TCX Data Analysis"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file", "Upload TCX file")
-    ),
-    mainPanel(
-      plotOutput("distance_plot")
+ui <- tagList(
+  tags$head(
+    tags$style(HTML("
+      .col-sm-4 {flex: 0 0 33.33333%; max-width: 33.33333%;}
+      .col-sm-8 {flex: 0 0 66.66667%; max-width: 66.66667%;}
+    "))
+  ),
+  fluidPage(
+    titlePanel("TCX Data Analysis"),
+    sidebarLayout(
+      sidebarPanel(
+        fileInput("file", "Upload TCX file"),
+        width = 4
+      ),
+      mainPanel(
+        plotOutput("distance_plot"),
+        leafletOutput("map"),
+        width = 8
+      )
     )
   )
 )
